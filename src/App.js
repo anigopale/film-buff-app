8de1f20ee0a5ee439a167bf057f5a2b7e2f13594
Aniruddh Gopale <anigopale@gmail.com>
2018-07-06 22:42:11 +0530

    create basic layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  font-size : 1.5rem;
+  max-width: 70rem;
+  min-height: 50rem;
+  margin: 4em auto;
+  background-color: #ecf0f1;
+  box-shadow: 0 2rem 6rem rgba(0, 0, 0, .3);
+`;
+
+const StyledHeader = styled.div`
+  height: 5rem;
+  background-color: #2c3e50;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <StyledApp>
+        <StyledHeader>
+
+        </StyledHeader>
+      </StyledApp>
     );
   }
 }
